2c21f49ec505fcc89c3efcb0749c294058e27474
Sean Dague <sean@dague.net>
2009-04-23 11:54:39 -0400

    add job direct show

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,5 +1,9 @@
 class JobsController < ApplicationController
     def index
-        @jobs = Bj.table.job.find(:all)  
+        @jobs = Bj.table.job.find(:all)
+    end
+    
+    def show
+        @jobs = Bj.table.job.find(params[:id])
     end
 end
