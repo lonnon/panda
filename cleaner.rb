b1b9ba8c305fb933dae4d1c43351b287a9de43e3
Sean Dague <sean@dague.net>
2009-04-27 17:48:30 -0400

    add a cleaner to fix hung issues

---
+++ cleaner.rb
@@ -0,0 +1,32 @@
+#!/usr/bin/env ./script/runner
+
+def pidofmono
+    IO.popen("ps auxw | grep -i mono | grep -i nant | grep test-xml | grep -v grep") do |line|
+        line.each do |l|
+            return l.split[1].to_i
+        end
+    end
+    return nil
+end
+
+
+def main
+    jobs = Bj.table.job.find(:all)
+    
+    jobs.each do |job|
+        if (job.state == "running") and (job.started_at < 30.minutes.ago)
+            # capture job so we can restart it
+            cmd = job.command
+            
+            pid = pidofmono
+            if pid 
+                Process.kill(2, pid)
+                sleep 5
+                Process.kill(9, pid)
+            end
+            Bj.sumbit cmd
+        end
+    end
+end
+
+main
