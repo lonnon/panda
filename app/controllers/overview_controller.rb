c75c3928c665fa8832bc42939f2a37077d5ba53e
Sean Dague <sean@dague.net>
2009-04-27 16:43:32 -0400

    I don't want anything that doesn't have a job id

--- app/controllers/overview_controller.rb
+++ app/controllers/overview_controller.rb
@@ -1,6 +1,6 @@
 class OverviewController < ApplicationController
     def index
-        @test = TestRun.find(:first, :order => "id desc")
+        @test = TestRun.find(:first, :conditions => ["job_id != ?", nil], :order => "id desc")
         @job = @test.job
         @revs = Revision.find(:all, :limit => 20, :order => "id desc").select {|r| r.test_runs.length > 0}
     end
