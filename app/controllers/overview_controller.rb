ed3055579dc59e3463fe8dc372bf96e4a1f7bb3f
Sean Dague <sean@dague.net>
2009-04-27 16:00:07 -0400

    added a much more reasonable overview controller

--- app/controllers/overview_controller.rb
+++ app/controllers/overview_controller.rb
@@ -1,5 +1,33 @@
 class OverviewController < ApplicationController
-  def index
-  end
-
+    def index
+        @job = last_job
+        @test = TestRun.find_by_job_id(@job.id)
+        @revs = Revision.find(:all, :limit => 20, :order => "id desc").select {|r| r.test_runs.length > 0}
+    end
+    
+    private
+   
+    def last_job
+        job = nil
+        jobs = Bj.table.job.find(:all)
+        if jobs.length > 0
+            if jobs.length == 1
+                return job = jobs[-1]
+            else
+                offset = -1
+                while((jobs[offset].state != "running") or (jobs[offset].state != "finished")) 
+                    offset -= 1
+                end
+                return jobs[offset]
+            end
+        end
+        
+        jobs = Bj.table.job_archive.find(:all)
+        if jobs.length > 0
+            return job = jobs[-1]
+        end
+        
+        return job
+    end
+    
 end
