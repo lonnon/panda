226161abe074a0c0ee2266099a6174985338c030
Sean Dague <sean@dague.net>
2009-04-27 16:07:04 -0400

    fix overview controller for most recent test run lookup

--- app/models/test_run.rb
+++ app/models/test_run.rb
@@ -5,6 +5,10 @@ class TestRun < ActiveRecord::Base
         begin
             return Bj.table.job.find(job_id)
         rescue => e
+            begin 
+                return Bj.table.job_archive(job_id)
+            rescue => e
+            end
             return nil
         end
     end
