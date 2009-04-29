43a7caa181c99ffbf4ca6971f3da6eac4c9e862c
Sean Dague <sean@dague.net>
2009-04-29 17:27:48 -0400

    added more detail on test show

--- app/models/test_run.rb
+++ app/models/test_run.rb
@@ -1,5 +1,7 @@
 class TestRun < ActiveRecord::Base
     belongs_to :revision
+    require "rexml/document"
+    
     
     def job
         begin
@@ -11,5 +13,24 @@ class TestRun < ActiveRecord::Base
             end
             return nil
         end
+    end
+    
+    def details
+        doc = to_rexml
+        results = Array.new
+        doc.root.elements.each do |res|
+            data = Hash.new
+            res.attributes.each do |k, v|
+                data[k] = v
+            end
+            data["name"].gsub!(/^.*\//, '')
+            results << data
+        end
+        return results
+    end
+    
+    private 
+    def to_rexml
+        REXML::Document.new "<xml>#{rawtest.gsub(/<\?xml.*?\?>/, '')}</xml>"
     end
 end
