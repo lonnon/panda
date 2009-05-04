198419c09ca52be2f9a9efbb8f58d1b4c7af5202
Sean Dague <sean@dague.net>
2009-05-04 14:41:57 -0400

    sort for the front page better

--- app/controllers/overview_controller.rb
+++ app/controllers/overview_controller.rb
@@ -2,6 +2,9 @@ class OverviewController < ApplicationController
     def index
         @test = TestRun.find(:first, :conditions => ["job_id is not null"], :order => "id desc")
         @job = @test.job
-        @revs = Revision.find(:all, :limit => 20, :order => "id desc").select {|r| r.test_runs.length > 0}
+        Repo.find(:all).each do |repo|
+            @revs << repo.revisions.find(:all, :limit => 20, :order => "time desc").select {|r| r.test_runs.length > 0}
+        end
+        @revs.sort_by {|r| r.time}
     end
 end
