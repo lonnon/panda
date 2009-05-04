7a77eb1a272c25cb8ac746a138cf60ee8dc30f76
Sean Dague <sean@dague.net>
2009-05-04 14:45:27 -0400

    flatten the revisions

--- app/controllers/overview_controller.rb
+++ app/controllers/overview_controller.rb
@@ -5,6 +5,7 @@ class OverviewController < ApplicationController
         @revs = Array.new
         Repo.find(:all).each do |repo|
             @revs << repo.revisions.find(:all, :limit => 20, :order => "time desc").select {|r| r.test_runs.length > 0}
+            @revs.flatten!
         end
         @revs.sort_by {|r| r.time}
     end
