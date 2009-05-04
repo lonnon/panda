c38b7c85c6e50b9749e4d84ed15c22ce860460e6
Sean Dague <sean@dague.net>
2009-05-04 14:42:42 -0400

    fix array issue

--- app/controllers/overview_controller.rb
+++ app/controllers/overview_controller.rb
@@ -2,6 +2,7 @@ class OverviewController < ApplicationController
     def index
         @test = TestRun.find(:first, :conditions => ["job_id is not null"], :order => "id desc")
         @job = @test.job
+        @revs = Array.new
         Repo.find(:all).each do |repo|
             @revs << repo.revisions.find(:all, :limit => 20, :order => "time desc").select {|r| r.test_runs.length > 0}
         end
