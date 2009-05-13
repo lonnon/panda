e96bfe70b94e005af57d6a6cbb29a37129d541ad
Sean Dague <sean@dague.net>
2009-05-13 14:09:34 -0400

    safety first

--- app/helpers/testruns_helper.rb
+++ app/helpers/testruns_helper.rb
@@ -1,9 +1,11 @@
 module TestrunsHelper
     def list_cores(rev)
         cores = []
-        Dir.foreach(rev.builddir) do |f|
-            if f =~ /^core.\d+/
-                cores << f
+        if Dir.exists?(rev.builddir)
+            Dir.foreach(rev.builddir) do |f|
+                if f =~ /^core.\d+/
+                    cores << f
+                end
             end
         end
         return cores
