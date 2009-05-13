417d9ca0fdbe0fc0ea6b4d0cf827cc32338bbe71
Sean Dague <sean@dague.net>
2009-05-13 15:37:39 -0400

    safety first

--- app/controllers/overview_controller.rb
+++ app/controllers/overview_controller.rb
@@ -1,8 +1,10 @@
 class OverviewController < ApplicationController
     def index
         @test = TestRun.find(:first, :conditions => ["job_id is not null"], :order => "id desc")
-        @job = @test.job
-        @revs = Array.new
+        if @test
+            @job = @test.job
+        end
+        @revs = []
         Repo.find(:all).each do |repo|
             @revs << repo.revisions.find(:all, :limit => 20, :order => "time desc").select {|r| r.test_runs.length > 0}
             @revs.flatten!
