05c1d17030aadf8cca00d6bc8fee8fdc41099381
Sean Dague <sean@dague.net>
2009-05-15 15:54:31 -0400

    simpler

--- app/models/job.rb
+++ app/models/job.rb
@@ -3,16 +3,4 @@ class Job < ActiveRecord::Base
     set_table_name "bj_job_archive"
     set_primary_key "bj_job_archive_id"
     
-        
-    def find(job_id)
-        begin
-            return Bj.table.job.find(job_id)
-        rescue => e
-            begin 
-                return find(job_id)
-            rescue => e
-            end
-            return nil
-        end
-    end
 end
