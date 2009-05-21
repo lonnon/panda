89fa550d11ccafacc6e69c394a2c71cacbcb8656
Sean Dague <sean@dague.net>
2009-05-21 16:10:33 -0400

    push out graph

---
+++ app/controllers/graph_controller.rb
@@ -0,0 +1,37 @@
+class GraphController < ApplicationController
+    def index
+        @graph = open_flash_chart_object(1000, 500, url_for(:controller => "graph", :action => "graph_code"))
+    end
+    
+    def graph_code
+        title = Title.new("Build Times")
+        bar = BarGlass.new
+        tests = TestRun.find(:all, :limit => "40", :order => "id desc")
+        
+        values = []
+        labels = []
+        tests.each do |test|
+            val = BarValue.new(test.runtime)
+            if test.success
+                val.set_colour("#00bb00") 
+            else
+                val.set_colour("#ff0000")
+            end
+            values << val
+            labels << XAxisLabel.new(test.revision.identifier, '#0000ff', 20, 'diagonal')
+        end
+        
+        x_labels = XAxisLabels.new
+        x_labels.labels = labels
+        x = XAxis.new
+        x.set_labels(x_labels)
+
+        bar.set_values(values)
+
+        chart = OpenFlashChart.new
+        chart.set_title(title)
+        chart.x_axis = x 
+        chart.add_element(bar)
+        render :text => chart.to_s
+    end
+end
