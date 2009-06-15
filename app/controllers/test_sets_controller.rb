0df3dd4b46f1050c7d726ce589a546e54f08550f
Sean Dague <sean@dague.net>
2009-06-15 13:52:44 -0400

    wrong object

--- app/controllers/test_sets_controller.rb
+++ app/controllers/test_sets_controller.rb
@@ -1,10 +1,10 @@
 class TestSetsController < ApplicationController
     def latest_rawxml
         test_set = TestSet.find(params[:id])
-        test_set.test_runs.find(:first, :order => "id desc")
+        run = test_set.test_runs.find(:first, :order => "id desc")
         
         respond_to do |format|
-            format.xml {render :xml => test_set.rawtest}
+            format.xml {render :xml => run.rawtest}
         end
     end
 end
