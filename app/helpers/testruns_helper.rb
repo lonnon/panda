117d7adbdf50fe2e12b20ee0e26f62bade6141ed
Sean Dague <sean@dague.net>
2009-07-02 10:29:43 -0400

    more safety checks

--- app/helpers/testruns_helper.rb
+++ app/helpers/testruns_helper.rb
@@ -1,7 +1,7 @@
 module TestrunsHelper
     def list_cores(test)
         cores = []
-        if File.directory? test.builddir
+        if File.directory? test.builddir and File.directory? "#{test.builddir}/bin"
             Dir.foreach "#{test.builddir}/bin" do |f|
                 if f =~ /^core.\d+/
                     cores << f
